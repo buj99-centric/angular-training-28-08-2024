015c07f193ed43c114dca44508d7d191d9f87189
Bujenita Claudiu <bujenitaclaudiu@gmail.com>
2024-08-30 12:19:08 +0300

    create template driven form for adding a new joke

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,22 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Joke } from '../joke-details/models/joke.model';
-import { JokeDetailsModule } from "../joke-details/joke-details.module";
+import { JokeDetailsModule } from '../joke-details/joke-details.module';
 import { ActivatedRoute } from '@angular/router';
 import { JokesService } from './jokes.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-joke-list',
   standalone: true,
-  imports: [CommonModule, JokeDetailsModule],
+  imports: [CommonModule, JokeDetailsModule, FormsModule],
   //providers: [JokesService],
   templateUrl: './joke-list.component.html',
-  styleUrl: './joke-list.component.scss'
+  styleUrl: './joke-list.component.scss',
 })
 export class JokeListComponent {
   jokes: Joke[] | undefined;
   joke: Joke | undefined;
-
+  newJokeModel = {
+    newJokeContent: '',
+    author: '',
+  };
   private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
   private jokesService: JokesService = inject(JokesService);
 
@@ -24,17 +28,11 @@ export class JokeListComponent {
     this.jokes = this.jokesService.getJokes();
 
     const params = this.activatedRoute.snapshot.params;
-    if(params['id']) {
+    if (params['id']) {
       this.joke = this.jokesService.getJokeById(params['id']);
     }
-
-    // this.activatedRoute.params.subscribe((params) => {
-    //   if(params['id']) {
-    //     this.joke = this.jokes?.find(joke => joke.id === params['id']);
-    //   }
-    // });
-
   }
-
-
+  addJoke(newJockForm: NgForm) {
+    console.log(newJockForm);
+  }
 }
--- src/app/joke-list/jokes.service.ts
+++ src/app/joke-list/jokes.service.ts
@@ -1,21 +1,39 @@
-import { Injectable } from "@angular/core";
-import { Joke } from "../joke-details/models/joke.model";
+import { Injectable } from '@angular/core';
+import { Joke } from '../joke-details/models/joke.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class JokesService {
-    private jokes: Joke[] = [
-        {id: '1', title: 'Joke 1', content: 'Chuck Norris does not fly coach OR first class. He travels by transcontinental flying sidekick.', date: new Date()} as Joke,
-        {id: '2',  title: 'Joke 2', content: 'As a kid, Chuck Norris\'s dog didn\'t eat his homework, his homework ate his dog', date: new Date()} as Joke,
-        {id: '3',  title: 'Joke 3', content: 'Little Miss Muffet sat on her tuffet, until Chuck Norris roundhouse kicked her into a glacier.', date: new Date()} as Joke,
-      ];
+  private jokes: Joke[] = [
+    {
+      id: '1',
+      title: 'Joke 1',
+      content:
+        'Chuck Norris does not fly coach OR first class. He travels by transcontinental flying sidekick.',
+      date: new Date(),
+    } as Joke,
+    {
+      id: '2',
+      title: 'Joke 2',
+      content:
+        "As a kid, Chuck Norris's dog didn't eat his homework, his homework ate his dog",
+      date: new Date(),
+    } as Joke,
+    {
+      id: '3',
+      title: 'Joke 3',
+      content:
+        'Little Miss Muffet sat on her tuffet, until Chuck Norris roundhouse kicked her into a glacier.',
+      date: new Date(),
+    } as Joke,
+  ];
 
-    getJokes(): Joke[] {
-        return this.jokes;
-    }
+  getJokes(): Joke[] {
+    return this.jokes;
+  }
 
-    getJokeById(id: string) {
-        return this.jokes?.find(joke => joke.id === id);
-    }
+  getJokeById(id: string) {
+    return this.jokes?.find((joke) => joke.id === id);
+  }
 }
