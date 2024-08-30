37425419521d3c052679a15700ba9ea99e4aaa92
Alexandru Tudose <alexandru.tudose@centric.eu>
2024-08-30 11:35:28 +0300

    Added services

--- src/app/joke-list/joke-list.component.ts
+++ src/app/joke-list/joke-list.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Joke } from '../joke-details/models/joke.model';
 import { JokeDetailsModule } from "../joke-details/joke-details.module";
 import { ActivatedRoute } from '@angular/router';
+import { JokesService } from './jokes.service';
 
 @Component({
   selector: 'app-joke-list',
   standalone: true,
   imports: [CommonModule, JokeDetailsModule],
+  //providers: [JokesService],
   templateUrl: './joke-list.component.html',
   styleUrl: './joke-list.component.scss'
 })
@@ -15,11 +17,15 @@ export class JokeListComponent {
   jokes: Joke[] | undefined;
   joke: Joke | undefined;
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.initilizeJokes();
-    const params = activatedRoute.snapshot.params;
+  private activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private jokesService: JokesService = inject(JokesService);
+
+  constructor() {
+    this.jokes = this.jokesService.getJokes();
+
+    const params = this.activatedRoute.snapshot.params;
     if(params['id']) {
-      this.joke = this.jokes?.find(joke => joke.id === params['id']);
+      this.joke = this.jokesService.getJokeById(params['id']);
     }
 
     // this.activatedRoute.params.subscribe((params) => {
@@ -30,11 +36,5 @@ export class JokeListComponent {
 
   }
 
-  initilizeJokes() {
-    this.jokes = [
-      {id: '1', title: 'Joke 1', content: 'Chuck Norris does not fly coach OR first class. He travels by transcontinental flying sidekick.', date: new Date()} as Joke,
-      {id: '2',  title: 'Joke 2', content: 'As a kid, Chuck Norris\'s dog didn\'t eat his homework, his homework ate his dog', date: new Date()} as Joke,
-      {id: '3',  title: 'Joke 3', content: 'Little Miss Muffet sat on her tuffet, until Chuck Norris roundhouse kicked her into a glacier.', date: new Date()} as Joke,
-    ];
-  }
+
 }
